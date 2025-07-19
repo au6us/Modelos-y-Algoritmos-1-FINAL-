4db1667ba1a54ff5f7ea5d4a752460e87a7d4574
Augus <145805991+au6us@users.noreply.github.com>
2025-07-19 20:29:05 -0300

    colocando sfx y system particles

--- Assets/Scripts/Player - MVC/PlayerView.cs
+++ Assets/Scripts/Player - MVC/PlayerView.cs
@@ -6,8 +6,26 @@ public class PlayerView : MonoBehaviour
     [Header("References")]
     [SerializeField] private PlayerModel model;
 
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource jumpSFX;
+    [SerializeField] private AudioSource doubleJumpSFX;
+    [SerializeField] private AudioSource hitSFX;
+    [SerializeField] private AudioSource dashSFX;
+    [SerializeField] private AudioSource footstepSFX;
+
+    [Header("Particle Systems")]
+    [SerializeField] private ParticleSystem jumpParticles;
+    [SerializeField] private ParticleSystem doubleJumpParticles;
+    [SerializeField] private ParticleSystem landParticles;
+    [SerializeField] private ParticleSystem dashParticles;
+
     private SpriteRenderer sr;
     private Animator anim;
+
+    private System.Action onJumpHandler;
+    private System.Action onDoubleJumpHandler;
+    private System.Action onLandHandler;
+    private System.Action onDamageHandler;
 
     private void Awake()
     {
@@ -17,58 +35,70 @@ public class PlayerView : MonoBehaviour
 
     private void OnEnable()
     {
-        model.OnJump += () => anim.SetBool("Jump", true);
-        model.OnDoubleJump += () => anim.SetBool("isDouble", true);
-        model.OnLand += ResetStatesOnLand;
-        model.OnDamage += () => SetHurt(true);
+        model.OnJump += () => {anim.SetBool("Jump", true);
+            jumpSFX.Play();
+            jumpParticles.Play();
+        };
+
+        model.OnDoubleJump += () => {anim.SetBool("isDouble", true);
+            doubleJumpSFX.Play();
+            doubleJumpParticles.Play();
+        };
+
+        model.OnLand += () => {ResetStatesOnLand();
+            landParticles.Play();
+        };
+
+        model.OnDamage += () => {SetHurt(true);
+            hitSFX.Play();
+
+        };
+
+        model.OnJump += onJumpHandler;
+        model.OnDoubleJump += onDoubleJumpHandler;
+        model.OnLand += onLandHandler;
+        model.OnDamage += onDamageHandler;
     }
 
     private void OnDisable()
     {
-        model.OnJump -= () => anim.SetBool("Jump", true);
-        model.OnDoubleJump -= () => anim.SetBool("isDouble", true);
-        model.OnLand -= ResetStatesOnLand;
-        model.OnDamage -= () => SetHurt(true);
+        model.OnJump -= onJumpHandler;
+        model.OnDoubleJump -= onDoubleJumpHandler;
+        model.OnLand -= onLandHandler;
+        model.OnDamage -= onDamageHandler;
     }
 
-    /// <summary>Actualiza Speed y voltea el sprite.</summary>
     public void HandleMove(Vector2 velocity)
     {
         anim.SetFloat("Speed", Mathf.Abs(velocity.x));
         sr.flipX = velocity.x < 0f;
     }
 
-    /// <summary>Marca salto (subida).</summary>
     public void SetJump(bool jumping)
     {
         anim.SetBool("Jump", jumping);
     }
 
-    /// <summary>Marca caída.</summary>
     public void SetFall(bool falling)
     {
         anim.SetBool("Fall", falling);
     }
 
-    /// <summary>Marca estado de suelo (enSuelo).</summary>
     public void SetGrounded(bool grounded)
     {
         anim.SetBool("isGround", grounded);
     }
 
-    /// <summary>Marca segundo salto.</summary>
     public void SetDouble(bool isDouble)
     {
         anim.SetBool("isDouble", isDouble);
     }
 
-    /// <summary>Marca daño (Hurt).</summary>
     public void SetHurt(bool hurt)
     {
         anim.SetBool("Hurt", hurt);
     }
 
-    /// <summary>Resetea estados al aterrizar (Jump, Double, Fall).</summary>
     public void ResetStatesOnLand()
     {
         anim.SetBool("Jump", false);
@@ -79,13 +109,11 @@ public class PlayerView : MonoBehaviour
         anim.SetBool("Hurt", false);
     }
 
-    /// <summary>Resetea dash tras la rutina.</summary>
     public void ResetDash()
     {
         anim.SetBool("Dash", false);
     }
 
-    /// <summary>Resetea Hurt tras la animación.</summary>
     public void ResetHurt()
     {
         anim.SetBool("Hurt", false);
